3f32e58c895102879418b7f41bc8104a16046b89
Alexandr Muratov <alexandr.muratov@eot.kiev.ua>
2012-11-22 13:42:56 +0200

    fixed

--- eot-update.sh
+++ eot-update.sh
@@ -4,10 +4,10 @@ dir=$my/$script
 echo = 1. Updating CORE
 cd $dir
 echo -n '  '
-git pull
+git pull origin master
 echo
 echo = 2. Updating modules
-for i in event_calendar file_tools widget_manager extended_tinymce embed_extender profile_manager izap-diskquota group_tools html_email_handler profileiconaccess editable_comments ; do
+for i in event_calendar file_tools widget_manager extended_tinymce embed_extender profile_manager izap-diskquota group_tools html_email_handler profileiconaccess editablecomments ; do
     echo === updating $i
     echo -n '    '
     cd $dir/mod/$i
