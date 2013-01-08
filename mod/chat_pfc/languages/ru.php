7139f3422f2fe7636e886d41423a2d654a487ec8
Alexandr Muratov <alexandr.muratov@eot.kiev.ua>
2013-01-08 15:53:39 +0200

    chat_pfc mod install & localization. chmod -R 777 src chmod -R 777 data commands required for starting the mod

---
+++ mod/chat_pfc/languages/ru.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Satheesh PM, BARC Mumbai
+ * www.satheesh.anushaktinagar.net
+*/
+
+
+
+$russian = array(
+'chat_pfc' => "Чат",
+'chat_pfc:chat' => "Чат",
+'chat_pfc:commands' => "Команды чата",
+'chat_pfc:list' =>'Список каналов',
+'chat_pfc:yes' => "Да",
+'chat_pfc:no' => "Нет",
+'chat_pfc:title' => "Введите название для вашего чата ",
+'chat_pfc:nikname' => "Разрешить пользователям менять ник во время чата",
+'chat_pfc:theme' => "Пожалуйста, выберите тему оформления для чата",
+'chat_pfc:blune' => 'blune',
+'chat_pfc:cerutti' => 'cerutti' ,
+'chat_pfc:default' => 'default',
+'chat_pfc:green' => 'green',
+'chat_pfc:msn' => 'msn',
+'chat_pfc:phoenity' =>  'phoenity',
+'chat_pfc:phpbb2' => 'phpbb2',
+'chat_pfc:zilveer' =>  'zilveer',
+'chat_pfc:img' => 'Показывать фото пользователя по клику на имени',
+'chat_pfc:maxlength' => "Введите максимальную длинну сообщения чата (например 140)",
+'chat_pfc:refresh' => "Введите задержку обновления содержания чата в миллисекундах (например 2000)",
+'chat_pfc:max_msg' => "Максимальное количество отображаемых сообщений чата (например 15)",
+'chat_pfc:max_room' => "Максимальное количество каналов, на которых пользователь может участвовать одновременно (например 10)",
+'chat_pfc:max_pvt' => "Максимальное количество приватных каналов для одного пользователя (например 5)",
+'chat_pfc:rooms' => 'Введите названия каналов (через запятую) (например мой чат, друзья, т.п.)',
+'chat_pfc:hight' => "Введите высоту основного окна чата (например 300)",
+'chat_pfc:isadmin' => "Должен ли первый пользователь, зашедший на канал, становиться его администратором?",
+'chat_pfc:chat_info' => '<b>Пожалуйста, сделайте все настройки перед переходом в чат</b><br />Если вы измените настройки позже, для их активации введите команду "/rehash" в чате.' ,
+'chat_pfc:support' => '<font color="red">If this pluggin is useful for you, Please do a <a href="http://community.elgg.org/plugins/883207/1.8.5" target="_blank">recommendation for this pluggin</a> in Elgg community so that others can also find this plugin easily.</font> For Support please send a Message Through My <a href="http://satheesh.anushaktinagar.net" target="_blank"><font color="blue"><b>Personal website</b></font></a>'
+
+);
+
+add_translation("ru",$russian);
